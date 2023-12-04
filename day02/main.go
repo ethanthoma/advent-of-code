5d122852bee6d816707ef22565ae42b633c9c636
Ethan Thoma <ethanthoma@gmail.com>
2023-12-03 22:23:33 -0800

    finished part 2

--- day02/main.go
+++ day02/main.go
@@ -9,19 +9,18 @@ import (
 )
 
 func main() {
-    maxCubes := MaxCubes{
-        RedCubes:   12,
-        GreenCubes: 13,
-        BlueCubes:  14,
-    }
-
-    gameId, err := processFile("./data/two.txt", maxCubes)
+//    val, err := task1("./data/two.txt", MaxCubes{
+//        RedCubes:   12,
+//        GreenCubes: 13,
+//        BlueCubes:  14,
+//    })
+    val, err := task2("./data/two.txt")
     if err != nil {
         fmt.Println("Error processing file:", err)
         return
     }
 
-    fmt.Println(gameId)
+    fmt.Println(val)
 }
 
 type MaxCubes struct {
@@ -30,66 +29,111 @@ type MaxCubes struct {
     BlueCubes   int
 }
 
-func processFile(filePath string, maxCubes MaxCubes) (int, error) {
-    file, err := os.Open(filePath)
-    if err != nil {
-        return 0, err
-    }
-    defer file.Close()
+func task1(filePath string, maxCubes MaxCubes) (int, error) {
+    sumOfGameIds := 0
 
-    possibleGames := 0 
-    scanner := bufio.NewScanner(file)
-    for scanner.Scan() {
-        line := scanner.Text()
+    lines, errChan := fileLines(filePath)
+    for line := range lines {
+        words := strings.Fields(line)
 
-        gameId, err := processLine(line, maxCubes)
+        gameId, err := strconv.Atoi(words[1][:len(words[1])-1])
         if err != nil {
             return 0, err
         }
-        
-        possibleGames += gameId
-    }
-    if err := scanner.Err(); err != nil {
-        return 0, err
-    }
 
-    return possibleGames, nil
-}
+        cubeCounts := map[string]int {
+            "red":   0,
+            "green": 0,
+            "blue":  0,
+        }
 
-func processLine(line string, maxCubes MaxCubes) (int, error) {
-    words := strings.Fields(line)
-
-    gameId, err := strconv.Atoi(words[1][:len(words[1])-1])
-    if err != nil {
-        return 0, err
-    }
-
-    cubeCounts := map[string]int {
-        "red":   0,
-        "green": 0,
-        "blue":  0,
-    }
-
-    for index, value := range words[2:] {
-        for color := range cubeCounts {
-            if strings.Contains(value, color) {
-                val, err := strconv.Atoi(words[index + 1])
-                if err != nil {
-                    return 0, err
+        for index, value := range words[2:] {
+            for color := range cubeCounts {
+                if strings.Contains(value, color) {
+                    val, err := strconv.Atoi(words[index + 1])
+                    if err != nil {
+                        return 0, err
+                    }
+                    cubeCounts[color] = max(cubeCounts[color], val)
                 }
-                cubeCounts[color] = max(cubeCounts[color], val)
             }
         }
+
+        impossibleGame := cubeCounts["red"] > maxCubes.RedCubes || 
+        cubeCounts["green"] > maxCubes.GreenCubes || 
+        cubeCounts["blue"] > maxCubes.BlueCubes
+
+        if !impossibleGame {
+            sumOfGameIds += gameId
+        }
+    }
+    if err := <-errChan; err != nil {
+        return 0, err
     }
 
-    impossibleGame := cubeCounts["red"] > maxCubes.RedCubes || 
-    cubeCounts["green"] > maxCubes.GreenCubes || 
-    cubeCounts["blue"] > maxCubes.BlueCubes
+    return sumOfGameIds, nil
+}
 
-    if impossibleGame {
-        return 0, nil
-    } else {
-        return gameId, nil
+func task2(filePath string) (int, error) {
+    sumOfPowerSet := 0
+
+    lines, errChan := fileLines(filePath)
+    for line := range lines {
+        words := strings.Fields(line)
+
+        cubeCounts := map[string]int {
+            "red":   0,
+            "green": 0,
+            "blue":  0,
+        }
+
+        for index, value := range words[2:] {
+            for color := range cubeCounts {
+                if strings.Contains(value, color) {
+                    val, err := strconv.Atoi(words[index + 1])
+                    if err != nil {
+                        return 0, err
+                    }
+                    cubeCounts[color] = max(cubeCounts[color], val)
+                }
+            }
+        }
+
+        sumOfPowerSet += cubeCounts["red"]*cubeCounts["green"]*cubeCounts["blue"]
     }
+    if err := <-errChan; err != nil {
+        return 0, err
+    }
+
+    return sumOfPowerSet, nil
+}
+
+func fileLines(filePath string) (<-chan string, <-chan error) {
+    lines := make(chan string)
+    errChan := make(chan error, 1)
+
+    go func() {
+        defer close(lines)
+        defer close(errChan)
+
+        file, err := os.Open(filePath)
+        if err != nil {
+            errChan <- err
+            return
+        }
+        defer file.Close()
+
+        scanner := bufio.NewScanner(file)
+        for scanner.Scan() {
+            lines <- scanner.Text()
+        }
+
+        if err := scanner.Err(); err != nil {
+            errChan <- err
+            return
+        }
+    }()
+
+    return lines, errChan
 }
 
